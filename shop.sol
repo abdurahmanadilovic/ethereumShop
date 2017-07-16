0863930fac603e3f4256783af94284ca6b1c8d4b
Abdu <kasoft1010@gmail.com>
2017-07-16 12:45:02 +0200

    add safety checks before item is purchased

--- shop.sol
+++ shop.sol
@@ -1,27 +1,26 @@
 pragma solidity ^0.4.11;
 
+// creeate own tokens, so that this contract can substract coins from purchasing address
 contract Shop{
     struct Item {
         uint price;
         address owner;
         bool forSale;
+        string name;
     }
-
     mapping (uint => Item) items;
-
+    event ItemSoled(uint itemId, address to);
     address owner;
     uint wallet;
     uint itemNumber;
-    // event ItemSoled(Item item, Shop to);
-    // event ItemBought(Item item, Shop from);
-    
+
     function Shop(){
         owner = msg.sender;
     }   
     
-    function addItem(uint price, address owner, bool forSale) returns (uint itemId){
+    function addItem(uint price, address owner, bool forSale, string name) returns (uint itemId) {
         itemId = itemNumber++;
-        items[itemId] = Item(price, owner, forSale);
+        items[itemId] = Item(price, owner, forSale, name);
     }
     
     function itemCount() returns (uint size){
@@ -31,18 +30,26 @@ contract Shop{
     function transferItem(uint itemId, address newOwner) payable returns (bool success) {
         Item memory itemToSell = items[itemId];
         
+        if (itemId >= itemNumber) {
+            return false;
+        }
         
-        itemToSell.owner = newOwner;
-        
-        // ItemSoled(itemToSell, to);
-        
-        return true;
+        if (newOwner.balance >= items[itemId].price) {
+            
+            items[itemId].owner.transfer(items[itemId].price);
+            itemToSell.owner = newOwner;
+            ItemSoled(itemId, newOwner);
+            
+            return true;
+        } else {
+            return false;
+        }
     }
     
-    function getItemsForOwner(address owner) returns (uint userItremsCount){
+    function getItemsForOwner(address owner) returns (uint userItremsCount) {
         uint userItemsCount = 0;
-        for (uint i=0; i<itemNumber; i++){
-            if(items[i].owner == owner){
+        for (uint i=0; i<itemNumber; i++) {
+            if (items[i].owner == owner) {
                 userItemsCount++;
             }
         }
