61ce3e23ca84e3767a65f1fe0db1ea894da4eab0
Abdu <kasoft1010@gmail.com>
2017-07-16 15:50:43 +0200

    create new coins to be used for item purchases

--- shop.sol
+++ shop.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.11;
 
-// creeate own tokens, so that this contract can substract coins from purchasing address
+// 0.000000000000000002 is equal to 2 eth on the remix web ide
+
 contract Shop{
     struct Item {
         uint price;
@@ -9,35 +10,50 @@ contract Shop{
         string name;
     }
     mapping (uint => Item) items;
+    mapping (address => uint) coinBank;
     event ItemSoled(uint itemId, address to);
     address owner;
     uint wallet;
     uint itemNumber;
-
+    uint totalCoins = 600000;
+    uint coinValuePerEth = 6000;
+    
     function Shop(){
         owner = msg.sender;
+        coinBank[owner] = totalCoins;
     }   
+    
+    function buyCoin() payable returns (bool success){
+        uint ethAmount = msg.value;
+        address buyer = msg.sender;
+        uint coinsBought = ethAmount * coinValuePerEth;
+        
+        if(coinBank[owner] >= coinsBought){
+            coinBank[owner] -= coinsBought;
+            coinBank[buyer] += coinsBought;
+            success = true;
+        }
+        else{
+            success = false;
+        }
+    }
     
     function addItem(uint price, address owner, bool forSale, string name) returns (uint itemId) {
         itemId = itemNumber++;
         items[itemId] = Item(price, owner, forSale, name);
     }
     
-    function itemCount() returns (uint size){
-        return itemNumber;
-    }
-    
-    function transferItem(uint itemId, address newOwner) payable returns (bool success) {
+    function transferItem(uint itemId, address newOwner) returns (bool success) {
         Item memory itemToSell = items[itemId];
         
         if (itemId >= itemNumber) {
             return false;
         }
         
-        if (newOwner.balance >= items[itemId].price) {
-            
-            items[itemId].owner.transfer(items[itemId].price);
-            itemToSell.owner = newOwner;
+        if (coinBank[newOwner] >= items[itemId].price) {
+            coinBank[newOwner] -= items[itemId].price;
+            coinBank[itemToSell.owner] += items[itemId].price;
+            items[itemId].owner = newOwner;
             ItemSoled(itemId, newOwner);
             
             return true;
